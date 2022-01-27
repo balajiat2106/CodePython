9179b0b62a1246b1eafdd51c5dcda288523fe638
Balaji S P <balaji.prabakaran87@gmail.com>
2022-01-27 21:08:31 +0530

    Second commit

--- newFile.py
+++ newFile.py
@@ -1,3 +1,16 @@
 print("Hello")
 c=input()
-print("Hello"+c)
+print("Hello"+str(c))
+
+temp=30
+if(temp>20):
+    print("hello")
+elif(temp<10):
+    print("Noooo")
+else:
+    print("nothing")
+
+i=1
+while i<=10:
+    print(i*"3")
+    i+=2
