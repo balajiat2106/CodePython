fd0aee238dff80796af6ad4b0700646691117a5f
Balaji S P <balaji.prabakaran87@gmail.com>
2022-02-10 23:58:59 +0530

    MaxProductOfThree

--- MaxProductOfThree.py
+++ MaxProductOfThree.py
@@ -1,15 +1,13 @@
 def solution(A):
     tempList=[]*3
-    tempList.append(max(A))
-    print(tempList)
-    A.remove(max(A))
-    print(A)
-    tempList.append(max(A))
-    print(tempList)
-    A.remove(max(A))
-    print(A)
-    tempList.append(max(A))
-    print(tempList)
-    A.remove(max(A))
-    print(A)
+    resProduct=0
+    for i in range(3):
+        tempList.append(max(A))
+        tempList[i]*=max(A)
+        print(tempList)
+        A.remove(max(A))
+        print(A)
+    
+
+    return resProduct
 print(solution([-3,1,2,-2,5,6]))
